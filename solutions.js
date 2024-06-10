fc92e6ff72a871b80ba27b6d9855d6f8396b9679
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-10 21:44:19 +0600

    Problem 34

--- solutions.js
+++ solutions.js
@@ -847,3 +847,22 @@ Expected
 function myFunction(a, b) {
   return b.split(a).length - 1;
 }
+
+
+// 34 Write a function that takes an object (a) as argument. Return the sum of all object values.
+
+/*
+myFunction({a:1,b:2,c:3})
+Expected
+6
+myFunction({j:9,i:2,x:3,z:4})
+Expected
+18
+myFunction({w:15,x:22,y:13})
+Expected
+50
+*/
+
+function myFunction(a) {
+  return Object.values(a).reduce((total, cv) => (cv += total), 0);
+}
