56a163855d1734231e4fcf36ec7dade90033817e
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-11 22:24:54 +0600

    Problem 35

--- solutions.js
+++ solutions.js
@@ -866,3 +866,21 @@ Expected
 function myFunction(a) {
   return Object.values(a).reduce((total, cv) => (cv += total), 0);
 }
+
+// 35. Write a function that takes an array of objects as argument. Sort the array by property b in ascending order. Return the sorted array
+
+/*
+myFunction([{a:1,b:2},{a:5,b:4}])
+Expected
+[{a:1,b:2},{a:5,b:4}]
+myFunction([{a:2,b:10},{a:5,b:4}])
+Expected
+[{a:5,b:4},{a:2,b:10}]
+myFunction([{a:1,b:7},{a:2,b:1}])
+Expected
+[{a:2,b:1},{a:1,b:7}]
+*/
+
+function myFunction(arr) {
+  return arr.sort((a, b) => a.b - b.b);
+}
