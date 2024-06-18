f6f023c1b14eeab2292a216cddff904ff61ccee2
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-18 20:52:06 +0600

    Problem 36

--- solutions.js
+++ solutions.js
@@ -884,3 +884,26 @@ Expected
 function myFunction(arr) {
   return arr.sort((a, b) => a.b - b.b);
 }
+
+
+
+// 36. Write a function that takes an array as argument. It should return true if all elements in the array are equal. It should return false otherwise.
+
+/*
+myFunction([true, true, true, true])
+Expected
+true 
+myFunction(['test', 'test', 'test'])
+Expected
+true 
+myFunction([1,1,1,2])
+Expected
+false 
+myFunction(['10',10,10,10])
+Expected
+false
+*/
+
+function myFunction(arr) {
+  return new Set(arr).size === 1;
+}
